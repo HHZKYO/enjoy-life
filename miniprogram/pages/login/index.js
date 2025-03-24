fef7445bf8a72ea191743a2851967c0904c29da3
HHZKYO <1692189552@qq.com>
2025-03-24 17:18:24 +0800

    用户管理-短信验证码-表单数据验证

--- miniprogram/pages/login/index.js
+++ miniprogram/pages/login/index.js
@@ -1,6 +1,17 @@
+// 导入 wechat-validate 模块
+import wxValidate from 'wechat-validate'
+
 Page({
+  behaviors: [wxValidate],
   data: {
     countDownVisible: false,
+  },
+
+  rules: {
+    mobile:[
+      {required: true, message: '请填写手机号码!'},
+      {pattern: /^1[3-8]\d{9}$/, message: '请填写正确的手机号码!'}
+    ]
   },
 
   countDownChange(ev) {
@@ -11,6 +22,11 @@ Page({
   },
   // 获取短信验证码
   getSMSCode() {
+    // 获取验证结果
+    const {valid, message} = this.validate('mobile')
+    // 如果验证不合法则不再执行后面的逻辑
+    if(!valid) return wx.utils.toast(message)
+    // 显示倒计时组件
     this.setData({countDownVisible: true})
   }
 })

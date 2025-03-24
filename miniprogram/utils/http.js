8124bf757808e3d5879f913c110ee557d5706d76
HHZKYO <1692189552@qq.com>
2025-03-24 20:00:51 +0800

    用户管理-头像和昵称-1

--- miniprogram/utils/http.js
+++ miniprogram/utils/http.js
@@ -4,6 +4,18 @@ import http from "wechat-http";
 // 配置接口基础路径
 http.baseURL = 'https://live-api.itheima.net'
 
+// 配置请求拦截器
+http.intercept.request = function (options) {
+  // 扩展头信息
+  const defaultHeader = {}
+  // 身份认证
+  defaultHeader.Authorization = 'Bearer ' + getApp().token
+  // 与默认头信息合并
+  options.header = Object.assign({}, defaultHeader, options.header)
+  // 处理后的请求参数
+  return options
+}
+
 // 配置响应拦截器
 http.intercept.response = function ({data}) {
   // 只保留 data 数据

a0adae16327e4395aed1026a6aa995aaca2d8b44
HHZKYO <1692189552@qq.com>
2025-03-24 15:01:23 +0800

    公告管理-公告列表

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -2,15 +2,25 @@
 // import utils from "../../utils/util"
 
 Page({
-  async onLoad() {
+  data: {
+    notices: []
+  },
+  onLoad() {
     // 模块方式调用
     // utils.toast('页面加载完成')
 
     // 全局对象调用
     // wx.utils.toast('页面加载完成')
 
+    // 获取公告列表数据
+    this.getNotices()
+  },
+  async getNotices() {
     // 请求接口数据（全局方式调用）
-    const res = await wx.http.get('/announcement')
-    console.log(res)
+    const {code, data: notices} = await wx.http.get('/announcement')
+    // 检测接口是否调用成功
+    if(code !== 10000) return wx.utils.toast()
+    // 渲染数据
+    this.setData({notices})
   }
 })

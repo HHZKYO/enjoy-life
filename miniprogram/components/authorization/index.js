7dde871823affbdbc4a194eaa70eee97c9948296
HHZKYO <1692189552@qq.com>
2025-04-01 10:19:55 +0800

    修复BUG-2

--- miniprogram/components/authorization/index.js
+++ miniprogram/components/authorization/index.js
@@ -24,11 +24,20 @@ Component({
       // 获取页面栈
       const pageStack = getCurrentPages()
       // 获取页面路径
-      const {route} = pageStack.pop()
+      const currentPage = pageStack.pop()
       // 未登录状态下跳转到登录页
       if(!isLogin) {
+        // 使用空白函数覆盖原生的生命周期 onLoad onShow
+        currentPage.onLoad = () => {}
+        currentPage.onShow = () => {}
+
+        // 判断 redirectURL 是不是 tabBar 页面
+        const isTabBarPage = wx.utils.isTabBarPage(currentPage.route)
+        // 如果是则使用 wx.switchTab
+        const routeType = isTabBarPage ? 'switchTab' : 'redirectTo'
+
         wx.redirectTo({
-          url: '/pages/login/index?redirectURL=/' + route,
+          url: `/pages/login/index?redirectURL=/${currentPage.route}&routeType=${routeType}`,
         })
       }
     }

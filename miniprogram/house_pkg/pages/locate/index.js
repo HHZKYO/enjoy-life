b80a22142069c59fb4d5265bb04b6189dbf1f923
HHZKYO <1692189552@qq.com>
2025-04-04 16:59:18 +0800

    可以搜寻到附近地址

--- miniprogram/house_pkg/pages/locate/index.js
+++ miniprogram/house_pkg/pages/locate/index.js
@@ -1,12 +1,41 @@
 // house_pkg/pages/locate/index.ts
+
+// 导入腾讯位置服务
+import QQMap from '../../../utils/qqmap'
+
 Page({
+  data: {
+    points: []
+  },
   onLoad() {
     this.getLocation()
   },
+
   // 获取用户位置
   async getLocation() {
     // 调用 API
     const {latitude, longitude} = await wx.getLocation()
-    console.log(latitude, longitude)
+    // console.log(latitude, longitude)
+    this.getPoint(latitude, longitude)
+  },
+
+  getPoint(latitude, longitude) {
+    // search 是实现地点搜索功能的方法
+    QQMap.search({
+      keyword: '住宅小区', //搜索关键词
+      location: [latitude, longitude].join(','), //设置周边搜索中心点
+      page_size: 5,
+      success: (result) => {
+        // 过滤掉多余的数据
+        const points = result.data.map(({ id, title, _distance }) => {
+          return { id, title, _distance }
+        })
+        // 渲染数据
+        this.setData({ points })
+      },
+      fail: (err) => {
+        console.log(err)
+      }
+    })
   }
 })

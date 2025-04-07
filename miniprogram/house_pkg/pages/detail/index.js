a014f4b3a2a01a54c589b28a63039f7b85ca2bf8
HHZKYO <1692189552@qq.com>
2025-04-07 16:15:36 +0800

    查询房屋详情

--- miniprogram/house_pkg/pages/detail/index.js
+++ miniprogram/house_pkg/pages/detail/index.js
@@ -1,4 +1,16 @@
 Page({
+  data: {
+    houseDetail: {},
+  },
+  onLoad({ id }) {
+    this.getHouseDetail(id)
+  },
+  async getHouseDetail(id) {
+    if (!id) return wx.utils.toast('参数有误')
+    const { code, data: houseDetail } = await wx.http.get('/room/' + id)
+    if (code !== 10000) return wx.utils.toast('获取房屋详情失败')
+    this.setData({ houseDetail })
+  },
   editHouse() {
     wx.navigateTo({
       url: '/house_pkg/pages/form/index',

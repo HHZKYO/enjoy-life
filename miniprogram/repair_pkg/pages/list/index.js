b408fffb281aa9171094c3300928a85adfa22973
HHZKYO <1692189552@qq.com>
2025-04-08 12:53:55 +0800

    报修列表

--- miniprogram/repair_pkg/pages/list/index.js
+++ miniprogram/repair_pkg/pages/list/index.js
@@ -1,4 +1,26 @@
 Page({
+  data: {
+    repairList: [],
+    isEmpty: false,
+  },
+  onLoad() {
+    this.getRepairList()
+  },
+  // 报修列表接口
+  async getRepairList() {
+    // 调用接口
+    const {
+      code,
+      data: { rows: repairList },
+    } = await wx.http.get('/repair', { current: 1, pageSize: 10 })
+    // 检测接口是否用成功
+    if (code !== 10000) return wx.utils.toast()
+    // 渲染数据
+    this.setData({
+      repairList,
+      isEmpty: repairList.length === 0,
+    })
+  },
   goDetail() {
     wx.navigateTo({
       url: '/repair_pkg/pages/detail/index',

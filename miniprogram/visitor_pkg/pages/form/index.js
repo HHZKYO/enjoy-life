3270643cd860222aa80747efe8760a77d0b5ee55
HHZKYO <1692189552@qq.com>
2025-04-08 16:57:27 +0800

    访客邀请-获取房屋列表&检测登录状态

--- miniprogram/visitor_pkg/pages/form/index.js
+++ miniprogram/visitor_pkg/pages/form/index.js
@@ -2,12 +2,28 @@ Page({
   data: {
     dateLayerVisible: false,
     houseLayerVisible: false,
-    houseList: [
-      { name: '北京西三旗花园1号楼 101' },
-      { name: '北京东村家园3号楼 302' },
-      { name: '北京育新花园3号楼 703' },
-      { name: '北京天通苑北苑8号楼 403' },
-    ],
+    houseList: []
+  },
+  onLoad() {
+    // 获取房屋列表
+    this.getHouseList()
+  },
+  // 获取房屋列表
+  async getHouseList() {
+    // 调用接口
+    const { code, data: houseList } = await wx.http.get('/house')
+    // 检测接口是否调用成功
+    if (code !== 10000) return wx.utils.toast()
+    // 渲染数据
+    this.setData({ houseList })
+  },
+  // 获取用户选择的房屋
+  selectHouseInfo(ev) {
+    // 记录获取的数据
+    this.setData({
+      houseId: ev.detail.id,
+      houseInfo: ev.detail.name,
+    })
   },
   openHouseLayer() {
     this.setData({ houseLayerVisible: true })

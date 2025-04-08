b448c3ce5b854d02755b54d147e7f96a34371e04
HHZKYO <1692189552@qq.com>
2025-04-08 17:30:35 +0800

    获取访客详情信息

--- miniprogram/visitor_pkg/pages/passport/index.js
+++ miniprogram/visitor_pkg/pages/passport/index.js
@@ -1,4 +1,22 @@
 Page({
+  data: {
+    passport: {}
+  },
+  onLoad({ id }) {
+    // 获取访客详情
+    this.getPassport(id)
+  },
+  // 获取访客详情（通行证）
+  async getPassport(id) {
+    // 检测是否存在 id
+    if (!id) return
+    // 调用接口
+    const { code, data: passport } = await wx.http.get('/visitor/' + id)
+    // 检测接口是否调用成功
+    if (code !== 10000) return wx.utils.toast()
+    // 渲染数据
+    this.setData({ passport })
+  },
   onShareAppMessage() {
     return {
       title: '查看通行证',

59ded862f2a9f310c8ba597cdbd40b252fa77e49
HHZKYO <1692189552@qq.com>
2025-04-08 21:02:37 +0800

    实现上拉加载功能

--- miniprogram/visitor_pkg/pages/list/index.js
+++ miniprogram/visitor_pkg/pages/list/index.js
@@ -1,3 +1,5 @@
+import { throttle } from 'miniprogram-licia'
+
 Page({
   data: {
     visitorList: [],
@@ -6,21 +8,28 @@ Page({
   onLoad() {
     // 获取访客列表
     this.getVisitorList()
+    this.getMoreVisitor = throttle(() => {
+      if (!this.data.hasMore) return
+      this.getVisitorList(++this._current)
+    }, 100)
   },
   // 访客列表接口
   async getVisitorList(current = 1, pageSize = 5) {
     // 调用接口
     const {
       code,
-      data: { rows: visitorList },
+      data: { pageTotal, rows: visitorList },
     } = await wx.http.get('/visitor', { current, pageSize })
     // 检测接口是否调用成功
     if (code !== 10000) return wx.utils.toast()
     // 渲染数据
     this.setData({
+      hasMore: current < pageTotal,
       isEmpty: visitorList.length === 0,
-      visitorList,
+      visitorList: this.data.visitorList.concat(visitorList),
     })
+    // 记录下来当前的页面
+    this._current = current
   },
   goPassport() {
     wx.navigateTo({

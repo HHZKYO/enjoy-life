b3611a71e15696f5d3ac0823f3ff8290e46f3384
HHZKYO <1692189552@qq.com>
2025-04-08 11:22:20 +0800

    在线报修-表单验证

--- miniprogram/repair_pkg/pages/form/index.js
+++ miniprogram/repair_pkg/pages/form/index.js
@@ -1,9 +1,15 @@
+import wxValidate from 'wechat-validate'
+
 Page({
+  behaviors: [wxValidate],
   data: {
     currentDate: Date.now(),
     houseLayerVisible: false,
     repairLayerVisible: false,
     dateLayerVisible: false,
+    mobile: '',
+    description: '',
+    appointment: '',
     houseList: [],
     repairItem: [],
     houseId: '',
@@ -13,6 +19,25 @@ Page({
     attachment: [
       { url: '/repair_pkg/static/uploads/attachment.jpg' },
       { url: '/repair_pkg/static/uploads/attachment.jpg' },
+    ],
+  },
+
+  rules: {
+    houseId: [
+      { required: true, message: '请选择报修房屋!' }
+    ],
+    repairItemId: [
+      { required: true, message: '请选择维修的项目!' }
+    ],
+    mobile: [
+      { required: true, message: '请填写手机号码!' },
+      { pattern: /^1[3-8]\d{9}$/, message: '请填写正确的手机号码!' },
+    ],
+    appointment: [
+      { required: true, message: '请选择预约日期!' }
+    ],
+    description: [
+      { required: true, message: '请填写问题描述!' }
     ],
   },
 
@@ -105,9 +130,7 @@ Page({
     })
   },
 
-  goList() {
-    wx.reLaunch({
-      url: '/repair_pkg/pages/list/index',
-    })
+  submitForm() {
+    this.validate()
   },
 })

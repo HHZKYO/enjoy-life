8362b3241b319234949b6642fd10a427a5ed5f41
HHZKYO <1692189552@qq.com>
2025-04-08 18:03:07 +0800

    让用户在未登录时也能查看访客详情数据

--- miniprogram/visitor_pkg/pages/passport/index.js
+++ miniprogram/visitor_pkg/pages/passport/index.js
@@ -2,10 +2,12 @@ Page({
   data: {
     passport: {}
   },
-  onLoad({ id }) {
+  onLoad({ id, encryptedData }) {
     // 获取访客详情
     this.getPassport(id)
+    this.getPassportShare(encryptedData)
   },
+
   // 获取访客详情（通行证）
   async getPassport(id) {
     // 检测是否存在 id
@@ -17,10 +19,24 @@ Page({
     // 渲染数据
     this.setData({ passport })
   },
+
+  async getPassportShare(encryptedData) {
+    // 检测是否存在 id
+    if (!encryptedData) return
+    // 调用接口
+    const { code, data: passport } = await wx.http.get('/visitor/share/' + encryptedData)
+    // 检测接口是否调用成功
+    if (code !== 10000) return wx.utils.toast()
+    // 渲染数据
+    this.setData({ passport })
+  },
+
   onShareAppMessage() {
+    // 获取加密数据
+    const { encryptedData } = this.data.passport
     return {
       title: '查看通行证',
-      path: '/visitor_pkg/pages/passport/index',
+      path: '/visitor_pkg/pages/passport/index?encryptedData=' + encryptedData,
       imageUrl: 'https://enjoy-plus.oss-cn-beijing.aliyuncs.com/images/share_poster.png',
     }
   },

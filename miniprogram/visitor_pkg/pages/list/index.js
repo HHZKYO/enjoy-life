0fc85ef9f87fbb07994ad21a31937bb9fd017aea
HHZKYO <1692189552@qq.com>
2025-04-08 20:47:49 +0800

    获取访客邀请记录的数据

--- miniprogram/visitor_pkg/pages/list/index.js
+++ miniprogram/visitor_pkg/pages/list/index.js
@@ -1,4 +1,27 @@
 Page({
+  data: {
+    visitorList: [],
+    isEmpty: false,
+  },
+  onLoad() {
+    // 获取访客列表
+    this.getVisitorList()
+  },
+  // 访客列表接口
+  async getVisitorList(current = 1, pageSize = 5) {
+    // 调用接口
+    const {
+      code,
+      data: { rows: visitorList },
+    } = await wx.http.get('/visitor', { current, pageSize })
+    // 检测接口是否调用成功
+    if (code !== 10000) return wx.utils.toast()
+    // 渲染数据
+    this.setData({
+      isEmpty: visitorList.length === 0,
+      visitorList,
+    })
+  },
   goPassport() {
     wx.navigateTo({
       url: '/visitor_pkg/pages/passport/index',

89090178237b8c840a8d3e45391879ee28604d37
HHZKYO <1692189552@qq.com>
2025-04-08 17:19:19 +0800

    访客邀请-数据验证

--- miniprogram/visitor_pkg/pages/form/index.js
+++ miniprogram/visitor_pkg/pages/form/index.js
@@ -1,12 +1,37 @@
+import wxValidate from 'wechat-validate'
+
 Page({
+  behaviors: [wxValidate],
   data: {
     name: '',
     gender: 1,
     mobile: '',
+    houseId: '',
+    visitDate: '',
     dateLayerVisible: false,
     houseLayerVisible: false,
-    houseList: []
+    houseList: [],
+    currentDate: Date.now(),
+    maxDate: Date.now() + 1000 * 3600 * 24 * 3,
   },
+
+  rules: {
+    name: [
+      { required: true, message: '访客姓名不能为空!' },
+      { pattern: /[\u4e00-\u9fa5]{2,5}/, message: '访客姓名只能为中文!' },
+    ],
+    mobile: [
+      { required: true, message: '访客手机号不能为空!' },
+      { pattern: /^1[3-8]\d{9}$/, message: '请填写正确的手机号码!' },
+    ],
+    houseId: [
+      { required: true, message: '请选择到访的房屋!' }
+    ],
+    visitDate: [
+      { required: true, message: '请选择到访的日期!' }
+    ],
+  },
+
   onLoad() {
     // 获取房屋列表
     this.getHouseList()
@@ -51,9 +76,22 @@ Page({
   closeDateLayer() {
     this.setData({ dateLayerVisible: false })
   },
-  goPassport() {
-    wx.reLaunch({
-      url: '/visitor_pkg/pages/passport/index',
-    })
-  },
+  // 提交表单数据
+  // async goPassport() {
+  //   // 验证表单数据
+  //   if (!this.validate()) return
+  //   // 获取接口需要的数据
+  //   const { name, gender, mobile, houseId, visitDate } = this.data
+  //   // 调用接口
+  //   const {
+  //     code,
+  //     data: { id },
+  //   } = await wx.http.post('/visitor', { name, gender, mobile, houseId, visitDate })
+  //   // 检测接口是否调用成功
+  //   if (code !== 10000) return wx.utils.toast()
+  //   // 跳转到访客详情页面
+  //   wx.reLaunch({
+  //     url: '/visitor_pkg/pages/passport/index?id=' + id,
+  //   })
+  // },
 })
